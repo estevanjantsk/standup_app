54043100493fbf9b34210271ef06ad648d182a20
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-10-15 17:31:39 +0300

    bin/importmap pin stimulus-dropdown --download

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "stimulus-dropdown" # @2.1.0
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "hotkeys-js" # @3.12.0
+pin "stimulus-use" # @0.51.3
