4e4535cb77fb620b2614dff5b355b13a9fcf08ee
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-10-15 18:32:27 +0300

    add User + self.implicit_order_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_15_150330) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_15_152511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
