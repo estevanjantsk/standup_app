dff46c0bcb4412d1b0064724a775554327f2fd48
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-10-17 10:31:41 +0300

    rails generate controller Users index show new create edit update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users, controllers: { registrations: "registrations" }
 
   resource :accounts
