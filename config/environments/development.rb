dd01ac8fe6fb933162ef598da29ec734c0f556ea
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-10-19 14:15:19 +0300

    users management

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,4 +78,9 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.asset_host = "http://localhost:#{ENV['PORT'] || 3000}"
+  config.action_mailer.default_url_options = { host: "http://localhost:#{ENV['PORT'] || 3000}" }
 end
